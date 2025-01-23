c88fa86aef63c50b964d8b42f98a24c54c7c5c8c
dachi <d_zedginidze@cu.edu.ge>
2025-01-22 21:08:46 -0500

    statemismatch

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import requests
 
 app = Flask('app')
 app.config['SESSION_COOKIE_SECURE'] = True
-app.config['SESSION_COOKIE_SAMESITE'] = 'None'
+app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
 app.config['SESSION_COOKIE_HTTPONLY'] = True
 app.config['SESSION_COOKIE_NAME'] = 'your_session_cookie'
 app.secret_key = os.environ.get('FLASK_SECRET_KEY')
