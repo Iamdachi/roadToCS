60e81e6b303e6699e1212109d7ab7d193b07ab9a
dachi <d_zedginidze@cu.edu.ge>
2025-01-24 20:52:23 -0500

    new auth5

--- app.py
+++ app.py
@@ -138,7 +138,7 @@ def oauth2callback():
     token_url, headers, body = client.prepare_token_request(
         token_endpoint,
         authorization_response=request.url,
-        redirect_url=request.base_url,
+        redirect_url="https://roadtocs.com/oauth2callback",
         code=code
     )
     token_response = requests.post(
