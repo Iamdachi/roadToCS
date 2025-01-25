a6f43911f851b19156e099657366df450fde79ac
dachi <d_zedginidze@cu.edu.ge>
2025-01-24 22:03:50 -0500

    new auth12

--- app.py
+++ app.py
@@ -141,6 +141,8 @@ def oauth2callback():
         redirect_url=request.base_url,
         code=code
     )
+    return(GOOGLE_CLIENT_ID)
+
     token_response = requests.post(
         token_url,
         headers=headers,
