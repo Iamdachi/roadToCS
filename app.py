e9867f7d02df6e9cfa3441f057f7978eb3f8bbe9
dachi <d_zedginidze@cu.edu.ge>
2025-01-24 21:13:22 -0500

    new auth7

--- app.py
+++ app.py
@@ -128,7 +128,7 @@ def signin():
 def oauth2callback():
     # Get authorization code Google sent back to you
     code = request.args.get("code")
-    return(code)
+
 
     # Find out what URL to hit to get tokens that allow you to ask for
     # things on behalf of a user
@@ -148,6 +148,7 @@ def oauth2callback():
         data=body,
         auth=(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET),
     )
+    return(token_response)
 
     # Parse the tokens!
     client.parse_request_body_response(json.dumps(token_response.json()))
