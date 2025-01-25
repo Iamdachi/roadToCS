3126a89701c55da7f61884e5bb3f72471a027505
dachi <d_zedginidze@cu.edu.ge>
2025-01-24 21:46:22 -0500

    new auth11

--- app.py
+++ app.py
@@ -116,7 +116,7 @@ def signin():
     request_uri = client.prepare_request_uri(
         authorization_endpoint,
         redirect_uri=request.host_url + "oauth2callback",
-        scope=["email"],
+        scope=["https://www.googleapis.com/auth/userinfo.email"],
     )
     return redirect(request_uri)
 
@@ -229,7 +229,7 @@ def get_lectures_data():
     with open('lectures.json') as f:
         lectures = json.load(f)
         if current_user.is_authenticated:
-            done_lectures = Lecture.query.join(user_lectures).join(User).filter(User.id == current_user.id).all()
+            done_lectures = Lecture.qadd_tokenuery.join(user_lectures).join(User).filter(User.id == current_user.id).all()
             for done_lecture in done_lectures:
                 name = done_lecture.name
                 cId = name.split('_')[0][1:]
